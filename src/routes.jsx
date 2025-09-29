5b2acc793d8def7c93625279f87eb70ea18b4665
VitorVraal <31941388+VitorVraal@users.noreply.github.com>
2025-09-29 16:12:50 -0300

    Tela minha evolução, mockup + preparação para API ( Mudanças serão feitas nas cores)

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,25 +5,19 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+
+import SignIn from "./pages/Auth/SignIn";
 import Login from "./pages/Auth/Login";
-import AdminDashboard from "./pages/Dashboard/AdminDashboard";
-import StudentDashboard from "./pages/Dashboard/StudentDashboard";
-import InstructorDashboard from "./pages/Dashboard/InstructorDashboard";
-import ReceptionistDashboard from "./pages/Dashboard/ReceptionistDashboard";
+import MinhaEvolucao from "./pages/Auth/MinhaEvolucao.jsx";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/signin" replace />} />
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/student/dashboard" element={<StudentDashboard />} />
-        <Route path="/instructor/dashboard" element={<InstructorDashboard />} />
-        <Route
-          path="/receptionist/dashboard"
-          element={<ReceptionistDashboard />}
-        />
+        <Route path="/minha-evolucao" element={<MinhaEvolucao />} />
       </Routes>
     </Router>
   );
